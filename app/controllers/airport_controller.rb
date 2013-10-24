071a622c003bf6f9a589977ade4d7a2fa1750cf5
kpy <kate@drkatewolin.com>
2013-10-24 07:05:25 -0500

    static page

--- app/controllers/airport_controller.rb
+++ app/controllers/airport_controller.rb
@@ -1,5 +1,15 @@
 class AirportController < ApplicationController
 
   def status
+    @airport_list = airport_list
   end
+
+
+  def airport_list
+    return  [
+      'ORD', 'LAX', 'BOS', 'MDW', 'PDX', 'AUS'
+    ]
+  end
+
+
 end
